3322d91e4855479d70f0269ae97e3332e371e4b0
ninggege <2543225600@qq.com>
2018-01-17 19:28:11 +0800

    常用代码
    常用 usually use

---
+++ lists/controller/Index.php
@@ -0,0 +1,27 @@
+<?php
+namespace app\lists\controller;
+use think\Controller;
+class Index extends Controller{
+	
+    public function index()
+    {
+
+        $page=input('page')?input('page'):1;
+        $db=db('student');
+        $pa=$db->paginate(config('paginate.list_rows'));
+        $sum=$db->field('id')->count();
+        $tiao=6;
+        $pages=ceil($sum/$tiao);
+        // if($page>$pages){
+        //     $page=$pages;
+        // }
+        // if($page<1){
+        //     $page=1;
+        // }
+        $list=$db->field("id,username,telphone,sex")->page($page,$tiao)->select();
+        $this->assign('page',$page);
+        $this->assign('list',$list);
+        $this->assign('pages',$pages);
+        return $this->fetch();
+    }
+}
